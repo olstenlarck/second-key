ecd15aeb8a79f83adc6040e5bd196a2dd1a530d5
Olsten Larck <olsten.larck@gmail.com>
2026-07-10 03:26:41 +0300

    Remove legacy API and callback rendering

--- src/pages/api/[...path].ts
+++
@@ -1,7 +0,0 @@
-import { env } from "cloudflare:workers";
-
-import { api } from "../../lib/server";
-
-import type { APIRoute } from "astro";
-
-export const ALL: APIRoute = ({ request, params }) => api(request, env as Env, params.path || "");
